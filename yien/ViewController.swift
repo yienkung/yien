f957c6a06f2370223995d82eef486e76fb979c72
magnus <bsd-04@BSD-04s-iMac.local>
2016-02-20 13:26:53 +0800

    test

--- yien/ViewController.swift
+++ yien/ViewController.swift
@@ -14,6 +14,43 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBOutlet weak var nameField: UITextField!
+    
+    var offset = 4
+    @IBOutlet weak var zodiacImage: UIImageView!
+    
+    @IBAction func pressButton(sender: UIButton) {
+        if let year = Int(nameField.text!){
+            let total = (year-offset)%4
+            zodiacImage.image = UIImage(named: String(total))
+            
+            
+            
+            
+        }
+        
+        
+        
+        
+    }
+    override func shouldAutorotate() -> Bool {
+        return false
+    }
+    
+    func keyboardoff(){
+        nameField.resignFirstResponder()
+        
+        
+    }
+    /*
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        keyboardoff()
+    }
+    
+    
+*/
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
